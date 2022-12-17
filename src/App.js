e3708c913f297d69791a576b3ca5412948511fbe
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-17 11:25:18 +0530

    get all posts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import "./App.css";
+import { useGetAllPostQuery } from "./services/post";
 
 function App() {
+    const responseInfo = useGetAllPostQuery();
+
+    console.log(responseInfo);
+    console.log(responseInfo.data);
+
+    if (responseInfo.isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (responseInfo.isError) {
+        return <div>{responseInfo.error.error}</div>;
+    }
+
     return (
-        <div className="App">
-            <h1>Hello, Arunesh</h1>
-        </div>
+        <>
+            <div>
+                {responseInfo.data.map((post, index) => {
+                    return (
+                        <div key={index}>
+                            <h3>
+                                {post.id} {post.title}
+                            </h3>
+                            <p>{post.body}</p>
+                            <hr />
+                        </div>
+                    );
+                })}
+            </div>
+        </>
     );
 }
 
